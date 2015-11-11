48330fa946d95f0825b842c4c1f22750c8861533
Maile Lucks <maile.lucks@gmail.com>
2015-11-10 23:43:27 -0800

    Append Changes

--- scripts.js
+++ scripts.js
@@ -2,17 +2,21 @@ $(document).ready(function() {
 
 function fizzBuzz(){
 	for (var i = 1; i <= 100; i++) {
-		if ((i % 3 === 0) && (i % 5 === 0)){ 
-			$('.list').append("<p>Fizz</p>");
+		if ( (i % 3 === 0) && (i % 5 === 0) ){ 
+			var output = "FizzBuzz";
+			$('.list').append(output + "<br/>");
 		}
 		else if (i % 5 === 0) {
-			$('.list').append("<p>Buzz</p>");
+			var output = "Buzz";
+			$('.list').append(output + "<br/>");
 		}
 		else if (i % 3 === 0) {
-			$('.list').append("<p>FizzBuzz</p>");
+			var output = "Fizz";
+			$('.list').append(output + "<br/>");
 		}
 		else {
-			$('.list').append("<p>" + i + "</p>");
+			var output = i;
+			$('.list').append(output + "<br/>");
 		}
 	};
 }
